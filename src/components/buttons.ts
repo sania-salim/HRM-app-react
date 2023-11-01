2bf634a6b74991577b8f5e066cf7a9340d6677ac
Sania Salim <sania@qburst.com>
2023-11-01 17:35:22 +0530

    initial commit

---
+++ src/components/buttons.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Regularbutton = styled.button`
+  width: 200px;
+  height: 50px;
+  color: violet;
+  border-color: violet;
+`;
+
+export default Regularbutton;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
+import Regularbutton from "./components/buttons.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Regularbutton>I'm a test button!</Regularbutton>
   </React.StrictMode>
 );
