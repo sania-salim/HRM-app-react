d11a4049a968f4bb77555ef54a45882fa02a6a30
Sania Salim <sania@qburst.com>
2023-11-02 17:42:55 +0530

    global style, button component, delete button

---
+++ src/components/buttons/buttons.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.button<{ deleteButton?: boolean }>`
+  color: ${(props) => (props.deleteButton ? "white" : "var(--primaryDark)")};
+  background-color: ${(props) =>
+    props.deleteButton ? "var(--themeRed)" : "var(--primaryLight)"};
+  width: 150px;
+  height: 40px;
+  border-radius: 3px;
+  border: none;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  &:hover,
+  &:focus {
+    color: white;
+    background-color: ${(props) =>
+      props.deleteButton ? "#8B0000" : "var(--primaryDark)"};
+  }
+`;
+
+export default Button;
---
+++ src/core/styles/global-styles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+:root {
+    --primaryDark: rgb(41, 93, 189);
+    --primaryLight: rgb(191, 212, 252);
+    --selection: rgb(168, 186, 219);
+    --accent: #ff9a03;
+    --themeWhite: #f5f5f5;
+    --themeBlack: #111111;
+    --themeGrey: #d9d9d9;
+    --themeRed: #ff0000;
+    --themeGreen: #5faf5e;
+  }
+  
+`;
+
+export default GlobalStyle;
