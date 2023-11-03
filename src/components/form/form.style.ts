14d1d72b1c241c7514be001665c58da8c5e87d99
Sania Salim <sania@qburst.com>
2023-11-03 18:54:35 +0530

    initial commit for forms

---
+++ src/components/form/form.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const InputHalfStyled = styled.input`
+  width: 240px;
+  height: 30px;
+`;
+
+export const InputFullStyled = styled.input`
+  width: 585px;
+  height: 30px;
+`;
+
+export const CustomDropDown = styled.div`
+  position: relative;
+`;
