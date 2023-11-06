30b7c0a5fd87cf835f1e727471a0495eaba93bed
Sania Salim <sania@qburst.com>
2023-11-06 13:09:06 +0530

    add user form

---
+++ src/add user/add-user.tsx
@@ -0,0 +1,12 @@
+import Form from "../components/form/form.js";
+
+function AddUser() {
+  return (
+    <>
+      <h2>Add new employee</h2>
+      <Form></Form>
+    </>
+  );
+}
+
+export default AddUser;
