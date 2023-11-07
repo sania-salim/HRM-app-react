4113cf42411ed0755c6437da7d360dd5bd4c7b58
Sania Salim <sania@qburst.com>
2023-11-07 13:16:27 +0530

    routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import GlobalStyle from "./core/styles/global-styles.ts";
-import Details from "./details/details.tsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./core/routing/routing.tsx";
 
 // import { ReactComponent as AddIcon } from "./assets/add.svg?react";
 
@@ -7,35 +8,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Details></Details>
+      <RouterProvider router={router} />
     </>
   );
 }
 
 export default App;
-
-{
-  /*
-import Button from "./components/buttons/button.tsx";
-import "./app.css";
-import Form from "./components/form/form.tsx";
-import Popup from "./components/popup/popup.tsx";
-  
-  <Button
-buttontype="regularButton"
-buttontext="I'm a regular test button"
-buttonicon=""
-/>
-<Button
-buttontype="deleteButton"
-buttontext="I'm a delete test button"
-buttonicon=""
-/>
-<Button
-buttontype="iconButton"
-buttontext="I'm an icon test button"
-buttonicon="src/assets/add.svg"
-/>
-<Form></Form>
-<Popup></Popup> */
-}
