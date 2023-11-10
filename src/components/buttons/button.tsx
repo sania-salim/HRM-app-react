357e0b4e95d2248ea2a914c6858544dae7fced67
Sania Salim <sania@qburst.com>
2023-11-10 13:42:26 +0530

    buttom icon conditionally rendered

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -3,7 +3,7 @@ import { ButtonStyled } from "./buttons.style";
 interface ButtonProps {
   buttontype: "deleteButton" | "iconButton" | "regularButton";
   buttontext: string;
-  buttonicon: string;
+  buttonicon?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -13,7 +13,7 @@ const Button: React.FC<ButtonProps> = ({
 }: ButtonProps) => {
   return (
     <ButtonStyled buttontype={buttontype}>
-      <img src={buttonicon} alt="" />
+      {buttonicon ? <img src={buttonicon} alt="" /> : ""}
       {buttontext}
     </ButtonStyled>
   );
