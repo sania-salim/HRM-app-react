f661f79da141b91699a6ef730a835f10ec2ccd47
Your Name <you@example.com>
2023-11-14 09:15:34 +0530

    edit prop

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -67,158 +67,152 @@ const Form: React.FC<FormProps> = ({ formtype }: FormProps) => {
       skills: [],
       workStatus: "",
     };
+  }
 
-    const formik = useFormik({
-      initialValues: initialvalues,
+  const formik = useFormik({
+    initialValues: initialvalues,
 
-      onSubmit: (values) => {
-        // object to push upon submission
-        const newEntry = {
-          id: 1006,
-          name: values.fullName,
-          designation: designationSelected,
-          mailID: values.mailID,
-          skills: selectedSkills,
-          workStatus: workOptionSelected,
-          location: locationOptionSelected,
-        };
+    onSubmit: (values) => {
+      // object to push upon submission
+      const newEntry = {
+        id: 1006,
+        name: values.fullName,
+        designation: designationSelected,
+        mailID: values.mailID,
+        skills: selectedSkills,
+        workStatus: workOptionSelected,
+        location: locationOptionSelected,
+      };
 
-        if (formtype === addForm) {
-          // employeeList.push(newEntry);
-        } else if (formtype === editForm) {
-          // deleteEmployee(newEntry.id);
-          // employeeList.push(newEntry);
-        }
-        console.log(newEntry);
-        console.log(employeeList);
-      },
+      if (formtype === addForm) {
+        // employeeList.push(newEntry);
+      } else if (formtype === editForm) {
+        // deleteEmployee(newEntry.id);
+        // employeeList.push(newEntry);
+      }
+      console.log(newEntry);
+      console.log(employeeList);
+    },
 
-      validationSchema,
-    });
+    validationSchema,
+  });
 
-    return (
-      <form onSubmit={formik.handleSubmit}>
-        <OuterFormContainer>
-          <img
-            src="src/assets/Profile photo.png"
-            alt="placeholder/profile image"
-            className="ProfilePhoto"
-          />
-          <InnerFormContainer>
-            <h3>Employee ID: 1001</h3>
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <OuterFormContainer>
+        <img
+          src="src/assets/Profile photo.png"
+          alt="placeholder/profile image"
+          className="ProfilePhoto"
+        />
+        <InnerFormContainer>
+          <h3>Employee ID: 1001</h3>
+          <FormFieldDivider>
+            <LabelForm>Full Name</LabelForm>
+            <InputFullStyled
+              type="text"
+              id="fullName"
+              name="fullName"
+              onChange={formik.handleChange}
+              value={formik.values.fullName}
+            ></InputFullStyled>
+            {formik.errors.fullName ? (
+              <ValidationError>{formik.errors.fullName}</ValidationError>
+            ) : null}
+          </FormFieldDivider>
+
+          <FormDivider>
             <FormFieldDivider>
-              <LabelForm>Full Name</LabelForm>
-              <InputFullStyled
-                type="text"
-                id="fullName"
-                name="fullName"
+              <LabelForm>Date of joining</LabelForm>
+              <InputHalfStyled
+                type="date"
+                id="dateOfJoining"
+                name="dateOfJoining"
                 onChange={formik.handleChange}
-                value={formik.values.fullName}
-              ></InputFullStyled>
-              {formik.errors.fullName ? (
-                <ValidationError>{formik.errors.fullName}</ValidationError>
+                value={formik.values.dateOfJoining}
+              ></InputHalfStyled>
+              {formik.errors.dateOfJoining ? (
+                <ValidationError>{formik.errors.dateOfJoining}</ValidationError>
               ) : null}
             </FormFieldDivider>
 
-            <FormDivider>
-              <FormFieldDivider>
-                <LabelForm>Date of joining</LabelForm>
-                <InputHalfStyled
-                  type="date"
-                  id="dateOfJoining"
-                  name="dateOfJoining"
-                  onChange={formik.handleChange}
-                  value={formik.values.dateOfJoining}
-                ></InputHalfStyled>
-                {formik.errors.dateOfJoining ? (
-                  <ValidationError>
-                    {formik.errors.dateOfJoining}
-                  </ValidationError>
-                ) : null}
-              </FormFieldDivider>
+            <FormFieldDivider>
+              <LabelForm>Date of birth</LabelForm>
+              <InputHalfStyled
+                type="date"
+                id="dateOfBirth"
+                name="dateOfBirth"
+                onChange={formik.handleChange}
+                value={formik.values.dateOfBirth}
+              ></InputHalfStyled>
+              {formik.errors.dateOfBirth ? (
+                <ValidationError>{formik.errors.dateOfBirth}</ValidationError>
+              ) : null}
+            </FormFieldDivider>
+          </FormDivider>
 
-              <FormFieldDivider>
-                <LabelForm>Date of birth</LabelForm>
-                <InputHalfStyled
-                  type="date"
-                  id="dateOfBirth"
-                  name="dateOfBirth"
-                  onChange={formik.handleChange}
-                  value={formik.values.dateOfBirth}
-                ></InputHalfStyled>
-                {formik.errors.dateOfBirth ? (
-                  <ValidationError>{formik.errors.dateOfBirth}</ValidationError>
-                ) : null}
-              </FormFieldDivider>
-            </FormDivider>
+          <h3>Contact details</h3>
+          <FormDivider>
+            <FormFieldDivider>
+              <LabelForm>Phone number</LabelForm>
+              <InputHalfStyled
+                type="text"
+                id="phoneNumber"
+                name="phoneNumber"
+                onChange={formik.handleChange}
+                value={formik.values.phoneNumber}
+              ></InputHalfStyled>
+              {formik.errors.phoneNumber ? (
+                <ValidationError>{formik.errors.phoneNumber}</ValidationError>
+              ) : null}
+            </FormFieldDivider>
 
-            <h3>Contact details</h3>
-            <FormDivider>
-              <FormFieldDivider>
-                <LabelForm>Phone number</LabelForm>
-                <InputHalfStyled
-                  type="text"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  onChange={formik.handleChange}
-                  value={formik.values.phoneNumber}
-                ></InputHalfStyled>
-                {formik.errors.phoneNumber ? (
-                  <ValidationError>{formik.errors.phoneNumber}</ValidationError>
-                ) : null}
-              </FormFieldDivider>
+            <FormFieldDivider>
+              <LabelForm>Mail ID</LabelForm>
+              <InputHalfStyled
+                type="email"
+                id="mailID"
+                name="mailID"
+                onChange={formik.handleChange}
+                value={formik.values.mailID}
+              ></InputHalfStyled>
+              {formik.errors.mailID ? (
+                <ValidationError>{formik.errors.mailID}</ValidationError>
+              ) : null}
+            </FormFieldDivider>
+          </FormDivider>
 
-              <FormFieldDivider>
-                <LabelForm>Mail ID</LabelForm>
-                <InputHalfStyled
-                  type="email"
-                  id="mailID"
-                  name="mailID"
-                  onChange={formik.handleChange}
-                  value={formik.values.mailID}
-                ></InputHalfStyled>
-                {formik.errors.mailID ? (
-                  <ValidationError>{formik.errors.mailID}</ValidationError>
-                ) : null}
-              </FormFieldDivider>
-            </FormDivider>
+          <h3>Employment details</h3>
+          <FormDivider>
+            <FormFieldDivider>
+              <CustomSelect></CustomSelect>
+            </FormFieldDivider>
 
-            <h3>Employment details</h3>
-            <FormDivider>
-              <FormFieldDivider>
-                <CustomSelect></CustomSelect>
-              </FormFieldDivider>
+            <FormFieldDivider>
+              <CustomSimpleSelect selectList="designation"></CustomSimpleSelect>
+            </FormFieldDivider>
+          </FormDivider>
+          <FormDivider>
+            <FormFieldDivider>
+              <CustomSimpleSelect selectList="workOptions"></CustomSimpleSelect>
+            </FormFieldDivider>
 
-              <FormFieldDivider>
-                <CustomSimpleSelect selectList="designation"></CustomSimpleSelect>
-              </FormFieldDivider>
-            </FormDivider>
-            <FormDivider>
-              <FormFieldDivider>
-                <CustomSimpleSelect selectList="workOptions"></CustomSimpleSelect>
-              </FormFieldDivider>
-
-              <FormFieldDivider>
-                <CustomSimpleSelect selectList="workLocation"></CustomSimpleSelect>
-              </FormFieldDivider>
-            </FormDivider>
-          </InnerFormContainer>
-        </OuterFormContainer>
-        <ButtonContainer>
-          <Button buttontype="regularButton" buttontext="Cancel" />
-          {formtype && editForm ? (
-            <Button
-              buttontype="deleteButton"
-              buttontext="Delete"
-              buttonicon=""
-            />
-          ) : (
-            ""
-          )}
-          <Button buttontype="regularButton" buttontext="Done" type="submit" />
-        </ButtonContainer>
-      </form>
-    );
-  }
+            <FormFieldDivider>
+              <CustomSimpleSelect selectList="workLocation"></CustomSimpleSelect>
+            </FormFieldDivider>
+          </FormDivider>
+        </InnerFormContainer>
+      </OuterFormContainer>
+      <ButtonContainer>
+        <Button buttontype="regularButton" buttontext="Cancel" />
+        {formtype && editForm ? (
+          <Button buttontype="deleteButton" buttontext="Delete" buttonicon="" />
+        ) : (
+          ""
+        )}
+        <Button buttontype="regularButton" buttontext="Done" type="submit" />
+      </ButtonContainer>
+    </form>
+  );
 };
 export default Form;
