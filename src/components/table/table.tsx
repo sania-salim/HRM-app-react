37c99f4a101071dffa6d3ee0e9ea02bcb0d20ddd
Your Name <you@example.com>
2023-11-21 09:20:16 +0530

    Mergeed with delivery-2

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -7,8 +7,6 @@ import {
 import { employeeList } from "../../core/config/constants";
 import { useNavigate } from "react-router-dom";
 
-import { tableContent } from "../../core/config/content";
-
 const tempObj = employeeList;
 
 function Table() {
@@ -33,29 +31,35 @@ function Table() {
   return (
     <>
       <TableStyled>
-        <TableRowStyled>
-          <TableHeaderStyled>ID</TableHeaderStyled>
-          <TableHeaderStyled>Name</TableHeaderStyled>
-          <TableHeaderStyled>Designation</TableHeaderStyled>
-          <TableHeaderStyled>Mail ID</TableHeaderStyled>
-          <TableHeaderStyled>Work status</TableHeaderStyled>
-          <TableHeaderStyled>Edit</TableHeaderStyled>
-        </TableRowStyled>
-
-        {tempObj.map((item) => (
+        <thead>
           <TableRowStyled>
-            <TableDataStyled>{item.id}</TableDataStyled>
-            <TableDataStyled>{item.name}</TableDataStyled>
-            <TableDataStyled>{item.designation}</TableDataStyled>
-            <TableDataStyled>{item.mailID}</TableDataStyled>
-            <TableDataStyled>
-              <img src="src/assets/work-from-home.svg" alt="" />
-            </TableDataStyled>
-            <TableDataStyled>
-              <img src="src/assets/edit button violet.svg" alt="" />
-            </TableDataStyled>
+            <TableHeaderStyled>ID</TableHeaderStyled>
+            <TableHeaderStyled>Name</TableHeaderStyled>
+            <TableHeaderStyled>Designation</TableHeaderStyled>
+            <TableHeaderStyled>Mail ID</TableHeaderStyled>
+            <TableHeaderStyled>Work status</TableHeaderStyled>
+            <TableHeaderStyled>Edit</TableHeaderStyled>
           </TableRowStyled>
-        ))}
+        </thead>
+        <tbody>
+          {tempObj.map((item) => (
+            <TableRowStyled
+              key={item.id}
+              onClick={(e) => navigateToPage(e, item.id)}
+            >
+              <TableDataStyled>{item.id}</TableDataStyled>
+              <TableDataStyled>{item.name}</TableDataStyled>
+              <TableDataStyled>{item.designation}</TableDataStyled>
+              <TableDataStyled>{item.mailID}</TableDataStyled>
+              <TableDataStyled>
+                <img src="src/assets/work-from-home.svg" alt="" />
+              </TableDataStyled>
+              <TableDataStyled>
+                <img src="src/assets/edit button violet.svg" alt="" />
+              </TableDataStyled>
+            </TableRowStyled>
+          ))}
+        </tbody>
       </TableStyled>
     </>
   );
